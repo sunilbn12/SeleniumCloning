44d14082d883cc1956a82f21e06e78ce4a4887ea
Sunil <Sunil@LAPTOP-4RU19F2E>
2019-05-30 09:29:08 +0530

    Committing

---
+++ Git/src/collections/HashMapToArray.java
@@ -0,0 +1,36 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+public class HashMapToArray {
+	
+	@Test
+	public void HM2Array()
+	{
+		//Creating a HashMap object 
+        
+        HashMap<String, String> studentPerformanceMap = new HashMap<String, String>(); 
+         
+        //Adding elements to HashMap 
+         
+        studentPerformanceMap.put("John Kevin", "Average"); 
+        studentPerformanceMap.put("Rakesh Sharma", "Good");         
+        studentPerformanceMap.put("Prachi D", "Very Good");          
+        studentPerformanceMap.put("Ivan Jose", "Very Bad");         
+        studentPerformanceMap.put("Smith Jacob", "Very Good");          
+        studentPerformanceMap.put("Anjali N", "Bad"); 
+        
+        //Getting keySet
+        ArrayList<String > keySet=new ArrayList<>(studentPerformanceMap.keySet());
+        
+        System.out.println("prinitng keySets -------------->"+keySet);
+        
+        //Getting values
+        ArrayList<String> values=new ArrayList<>(studentPerformanceMap.values());
+        System.out.println("prinitng values -------------->"+values);
+	}
+
+}
